200334244f46485e26eed60c03f1e5ec22ee6f3c
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-09-10 17:16:27 -0300

    chore: use standard as base for eslint and adjust prettier config

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,9 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-jest',
+    '.+\\.ts$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
-  },
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 };
