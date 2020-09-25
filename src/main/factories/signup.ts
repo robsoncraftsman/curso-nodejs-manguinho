ed01cf40155dc03e3b1b3bdd554d932c2beefb19
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-09-25 18:59:42 -0300

    feat: update SignUpController factory to use new decorators

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,11 +4,17 @@ import { DbAddAccount } from '../../data/usecases/db-add-account';
 import { BCryptAdapter } from '../../infra/cryprography/bcrypt-adpater';
 import { AccountMongoDbRepository } from '../../infra/db/mongodb/repositories/AccountMongoDbRepository';
 import env from '../config/env';
+import { ResponseErrorControllerDecorator } from '../decorators/response-error-controller-decorator';
+import { LogControllerDecorator } from '../decorators/log-controller-decorator';
+import { Controller } from '../../presentation/protocols';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const emailValidatorAdapter = new EmailValidatorAdapter();
   const bcrypAdapter = new BCryptAdapter(env.SALT);
   const accountMongoDbRepository = new AccountMongoDbRepository();
   const dbAddAccount = new DbAddAccount(bcrypAdapter, accountMongoDbRepository);
-  return new SignUpController(emailValidatorAdapter, dbAddAccount);
+  const signupController = new SignUpController(emailValidatorAdapter, dbAddAccount);
+  const logControllerDecorator = new LogControllerDecorator(signupController);
+  const responseErrorControllerDecorator = new ResponseErrorControllerDecorator(logControllerDecorator);
+  return responseErrorControllerDecorator;
 };
