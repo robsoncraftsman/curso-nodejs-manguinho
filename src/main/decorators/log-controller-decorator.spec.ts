a13c25e7fbacd18751462f836b9733e2ce52233d
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-09-25 18:42:20 -0300

    feat: add LogControllerDecorator

---
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -0,0 +1,28 @@
+import { ServerError } from '../../presentation/errors';
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols';
+import { LogControllerDecorator } from './log-controller-decorator';
+import { serverError } from '../../presentation/helpers/http-helper';
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      return new Promise((resolve) => resolve(serverError()));
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): LogControllerDecorator => {
+  const controllerStub = makeControllerStub();
+  return new LogControllerDecorator(controllerStub);
+};
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    const sut = makeSut();
+    const httpRequest = { body: 'OK' };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
+});
