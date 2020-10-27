9482a9a804cc61f26cdfe895b684fac4bf435ab2
Robson S. Rodrigues <robsoncraftsman@gmail.com>
2020-10-27 15:54:05 -0300

    test: add new tests for LogControllerDecorator

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -1,25 +1,53 @@
 import { ServerError } from '../../presentation/errors';
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log-controller-decorator';
-import { serverError } from '../../presentation/helpers/http-helper';
+import { ok, serverError } from '../../presentation/helpers/http-helper';
+
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
 
 const makeControllerStub = (): Controller => {
   class ControllerStub implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-      return new Promise((resolve) => resolve(serverError(new Error())));
+      return new Promise((resolve) => resolve(ok('OK')));
     }
   }
   return new ControllerStub();
 };
 
-const makeSut = (): LogControllerDecorator => {
+const makeSut = (): SutTypes => {
   const controllerStub = makeControllerStub();
-  return new LogControllerDecorator(controllerStub);
+  const sut = new LogControllerDecorator(controllerStub);
+  return {
+    sut,
+    controllerStub
+  };
 };
 
 describe('LogController Decorator', () => {
   test('Should call controller handle', async () => {
-    const sut = makeSut();
+    const { sut, controllerStub } = makeSut();
+    const handleSpy = jest.spyOn(controllerStub, 'handle');
+    const httpRequest = { body: 'OK' };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenLastCalledWith(httpRequest);
+  });
+
+  test('Should return the same httpResponse', async () => {
+    const { sut } = makeSut();
+    const httpRequest = { body: 'OK' };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual('OK');
+  });
+
+  test('Should return a server error', async () => {
+    const { sut, controllerStub } = makeSut();
+    jest
+      .spyOn(controllerStub, 'handle')
+      .mockReturnValueOnce(new Promise((resolve) => resolve(serverError(new Error()))));
     const httpRequest = { body: 'OK' };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(500);
